9cef704eda3ab6a9986a5ef19fc1453c34e8bd18
Thomas Nakagawa <thomasnakagawa@gmail.com>
2022-03-05 23:50:42 -0800

    Add link in about

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -5,6 +5,9 @@ export default function Aboutpage(): JSX.Element {
   return (
     <>
       <h1>About</h1>
+      <a href='https://github.com/thomasnakagawa/word-puzzle-create'>source</a>
+      <br/>
+      <br/>
       <Link to='/'>back</Link>
     </>
   );
