9604269180bfbf3986ba65d1f574f15c4391ac22
Thomas Nakagawa <thomasnakagawa@gmail.com>
2022-03-13 16:47:49 -0700

    Add fetch function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,8 +2,10 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import onProcessGuess from './callables/onProcessGuess';
 import onWritePuzzle from "./callables/onWritePuzzle";
+import onFetchPuzzlePrompt from "./callables/onFetchPuzzlePrompt";
 
 admin.initializeApp(functions.config().firebase);
 
 export const processGuess = functions.https.onCall(onProcessGuess);
 export const writePuzzle = functions.https.onCall(onWritePuzzle);
+export const fetchPuzzlePrompt = functions.https.onCall(onFetchPuzzlePrompt);
